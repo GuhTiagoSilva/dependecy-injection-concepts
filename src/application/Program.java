d4c0f01fd3637502ef6fe77ae6014d96c93a2ecd
GuhTiagoSilva <46786490+GuhTiagoSilva@users.noreply.github.com>
2021-04-04 21:02:09 -0300

    First Dependency Injection implementation - Constructors

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,11 +1,16 @@
 package application;
 
+import services.DeliverService;
 import services.PayService;
+import services.TaxService;
 
 public class Program {
 
 	public static void main(String[] args) {
-		PayService payService = new PayService();
+		TaxService taxService = new TaxService();
+		DeliverService deliverService = new DeliverService();
+		
+		PayService payService = new PayService(taxService, deliverService);
 		double finalPrice = payService.finalPrice(300.0, "SC");
 		System.out.println("Result = " + finalPrice);
 	}
--- src/services/PayService.java
+++ src/services/PayService.java
@@ -2,8 +2,13 @@ package services;
 
 public class PayService {
 
-	private TaxService taxService = new TaxService();
-	private DeliverService deliverService = new DeliverService();
+	private TaxService taxService;
+	private DeliverService deliverService;
+	
+	public PayService(TaxService taxService, DeliverService deliverService) {
+		this.taxService = taxService;
+		this.deliverService = deliverService;
+	}
 
 	public double finalPrice(double cost, String state) {
 		return cost + deliverService.fee(state) + taxService.tax(cost);
